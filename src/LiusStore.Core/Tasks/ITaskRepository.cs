81272cc97ebb742f37b13823fa452df9c6450a7f
Pengfang Liu <lpf_mart@163.com>
2020-08-11 08:26:21 +0800

    自定义数据仓储

---
+++ src/LiusStore.Core/Tasks/ITaskRepository.cs
@@ -0,0 +1,12 @@
+﻿using Abp.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LiusStore.Tasks
+{
+    public interface ITaskRepository : IRepository<Task, long>
+    {
+        List<Task> GetAllWithPeople(int? assignedPersonId, TaskState? state);
+    }
+}
