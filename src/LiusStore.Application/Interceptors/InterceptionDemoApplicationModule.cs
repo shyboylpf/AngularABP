191a5bc0a5389328fdd8dc22b63f3bf23b12b76b
Pengfang Liu <lpf_mart@163.com>
2020-08-17 16:12:00 +0800

    注册拦截器到IApplicationService的实例

---
+++ src/LiusStore.Application/Interceptors/InterceptionDemoApplicationModule.cs
@@ -0,0 +1,15 @@
+﻿using Abp.Modules;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LiusStore.Interceptors
+{
+    public class InterceptionDemoApplicationModule : AbpModule
+    {
+        public override void PreInitialize()
+        {
+            MeasureDurationInterceptorRegistrar.Initialize(IocManager.IocContainer.Kernel);
+        }
+    }
+}
---
+++ src/LiusStore.Application/Interceptors/MeasureDurationInterceptorRegistrar.cs
@@ -0,0 +1,26 @@
+﻿using Abp.Application.Services;
+using Castle.Core;
+using Castle.MicroKernel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LiusStore.Interceptors
+{
+    internal class MeasureDurationInterceptorRegistrar
+    {
+        public static void Initialize(IKernel kernel)
+        {
+            kernel.ComponentRegistered += Kernel_ComponentRegistered;
+        }
+
+        private static void Kernel_ComponentRegistered(string key, IHandler handler)
+        {
+            if (typeof(IApplicationService).IsAssignableFrom(handler.ComponentModel.Implementation))
+            {
+                handler.ComponentModel.Interceptors.Add
+                (new InterceptorReference(typeof(MeasureDurationInterceptor)));
+            }
+        }
+    }
+}
