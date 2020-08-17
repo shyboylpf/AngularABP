7cee0a5d76edb1a4b73599a44a0e9969d1dfd283
Pengfang Liu <lpf_mart@163.com>
2020-08-17 15:42:38 +0800

    监控方法执行时长的aop方法 , 使用 Castle Windsor

--- src/LiusStore.Application/Interceptors/MeasureDurationInterceptor.cs
+++ src/LiusStore.Application/Interceptors/MeasureDurationInterceptor.cs
@@ -1,15 +1,35 @@
-﻿using Castle.DynamicProxy;
+﻿using Castle.Core.Logging;
+using Castle.DynamicProxy;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 
 namespace LiusStore.Interceptors
 {
     class MeasureDurationInterceptor : IInterceptor
     {
+        public ILogger Logger { get; set; }
+        public MeasureDurationInterceptor()
+        {
+            Logger = NullLogger.Instance;
+        }
         public void Intercept(IInvocation invocation)
         {
-            throw new NotImplementedException();
+            // Before methond execution
+            // 方法执行前
+            var stopwatch = Stopwatch.StartNew();
+
+            // Executing the  actual method
+            // 执行方法
+            invocation.Proceed();
+
+            // After method execution.
+            // 方法执行结束
+            stopwatch.Stop();
+            Logger.InfoFormat("MeasureDurationInterceptor: {0} executed in {1} milliseconds.",
+                invocation.MethodInvocationTarget.Name,
+                stopwatch.ElapsedMilliseconds.ToString("0.000"));
         }
     }
 }
